c714cbb2d8e3c3116866762de95609c10fbdfbbc
Fabiano Antunes <182300640@senacinformatica.br>
2024-03-12 21:16:29 -0300

    Inicial tipo de dados

--- firstprg.py
+++ firstprg.py
@@ -1,1 +1,17 @@
 print ("Olá mundo")
+
+#Variáveis
+nome = "Fabiano"
+idade = 50
+print(nome, idade)
+
+#Tipo de dados
+numero_inteiro = 10
+numero_real = 3.14
+texto = "Olá, mudo!"
+
+print(type(numero_inteiro))
+print(type(numero_real))
+print(type(texto))
+
+#Operadores
