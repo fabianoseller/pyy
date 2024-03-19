aa58a18598de9bb7dd0e4d04b2802354e301f462
Fabiano Antunes <42979563+fabianoseller@users.noreply.github.com>
2024-03-19 11:12:34 -0300

    Arruamando valores_soma.py da aula

--- aul20240318/tuplas/valores_soma.py
+++ aul20240318/tuplas/valores_soma.py
@@ -1,19 +1,15 @@
 lista = [(3, 8, -1, 2), (2, -2, 4, 2), (7, 5, 3, -9)]
 valores = []
-#Ou seja, criou-se uma tupla com os valores no índice 
-#0 de cada lista, outra com os valores do índice
-#1 e outra com os valores no índice 2. 
-# Basta agora somarmos os valores quando os mesmos não 
-# forem negativos, armazenando o resultado em variáveis, utilizando a função nativa sum.
+
+# Calculando a soma dos valores não negativos em cada tupla e armazenando na lista 'valores'
 sum_1 = sum(i for i in lista[0] if i >= 0) # Resulta: 13
 sum_2 = sum(i for i in lista[1] if i >= 0) # Resulta: 8
 sum_3 = sum(i for i in lista[2] if i >= 0) # Resulta: 15
 
-#Ou seja, criou-se uma tupla com os valores no índice 0 de cada lista,
-#outra com os valores do índice 1 e outra com os valores no índice 2. 
-#Basta agora somarmos os valores quando os mesmos não forem negativos, 
-#armazenando o resultado em variáveis, utilizando a função nativa sum.
+valores.append(sum_1)
+valores.append(sum_2)
+valores.append(sum_3)
 
-print("A sum_1: {}".format(valores[sum_1]))
-print("A sum_2: {}".format(valores[sum_2]))
-print("A sum_3: {}".format(valores[sum_3]))
+print("A sum_1: {}".format(sum_1))
+print("A sum_2: {}".format(sum_2))
+print("A sum_3: {}".format(sum_3))
