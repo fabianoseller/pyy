f144a53181bd2ebd657e86cd4bcb40655ac5e80c
Fabiano Antunes <182300640@senacinformatica.br>
2024-03-28 21:15:30 -0300

    Exercicios conjunjtos aula 14 com referencias web

--- aula14/intersecao.py
+++ aula14/intersecao.py
@@ -16,3 +16,8 @@ planetas1 = {'Vênus', 'Mercúrio', 'Terra2', 'Netuno', 'Plutao'}
 planetas2 = {'Júpiter', 'Terra', 'Urano', 'Saturno', 'Marte'}
 print(planetas1 & planetas2)
 print(planetas1.intersection(planetas2))
+
+#https://pythonhelp.wordpress.com/2013/06/18/conjuntos-em-python/
+
+#https://bosontreinamentos.com.br/programacao-em-python/sets-conjuntos-em-python/
+
