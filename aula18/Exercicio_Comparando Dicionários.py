09358ed47f10e6f22a219867fb1119b8212da8a3
Fabiano Antunes <182300640@senacinformatica.br>
2024-04-05 21:33:15 -0300

    Aula em 05042024 dicionarios 5 exercicios com 5

---
+++ aula18/Exercicio_Comparando Dicionários.py
@@ -0,0 +1,9 @@
+# https://iaexpert.academy/2019/09/12/dicionarios-em-python/
+#  Crie dois dicionários com as informações de dois alunos e compare seus 
+# nomes e idades.
+# • Crie dois dicionários com as informações de dois produtos e compare seus 
+# preços.
+# • Crie dois dicionários com as informações de dois livros e compare seus 
+# autores.
+# • Crie dois dicionários com as informações de dois filmes e compare seus 
+# diretores.
