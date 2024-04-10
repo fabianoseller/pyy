01f6be5a4f6e8aae41f76b6664b03090e9a883f6
Fabiano Antunes <182300640@senacinformatica.br>
2024-04-09 21:30:42 -0300

    pasta aula 19 Modulos e pacotes

---
+++ aula19/Modulos/importacaoespecifica.py
@@ -0,0 +1,12 @@
+from meu_modulo import soma
+
+
+resultado_soma = soma(5, 3)
+
+
+print(f"Soma: {resultado_soma}")  # Imprime 8
+
+
+#Arquivos Python (.py) que contêm definições como funções, classes e variáveis.
+#Permitem organizar o código em unidades modulares e reutilizáveis.
+#Para usar um módulo em outro script, utilize a instrução import.
---
+++ aula19/Modulos/main.py
@@ -0,0 +1,12 @@
+import meu_modulo 
+
+resultado_soma = meu_modulo.soma(5, 3)
+resultado_subtracao = meu_modulo.subtracao(10, 4)
+
+print(f"Soma: {resultado_soma}")  # Imprime 8
+print(f"Subtração: {resultado_subtracao}")  # Imprime 6
+
+
+#Arquivos Python (.py) que contêm definições como funções, classes e variáveis.
+#Permitem organizar o código em unidades modulares e reutilizáveis.
+#Para usar um módulo em outro script, utilize a instrução import.
