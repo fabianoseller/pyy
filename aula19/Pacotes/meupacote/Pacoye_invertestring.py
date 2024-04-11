afde3e204a6e2bf3396956c15f524c98a6361de6
Fabiano Antunes <182300640@senacinformatica.br>
2024-04-10 21:23:17 -0300

    Aqui arrumando exercicio de funções

--- aula19/Pacotes/meupacote/Pacoye_invertestring.py
+++ aula19/Pacotes/meupacote/Pacoye_invertestring.py
@@ -1,4 +1,4 @@
 def inverter_string(s):
     return s[::-1]
 
-print(inverter_string("Hello mundo metaverso ...aqui ficou mais simples"))  # Saída: "olleH"
+print(inverter_string("Hello mundo metaverso ...aqui ficou mais simples"))  # Saída: "osrevatem odnum olleH"
