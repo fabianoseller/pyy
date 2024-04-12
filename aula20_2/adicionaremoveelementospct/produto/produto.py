ead1315b59e6fef803a16498141ab11dd0308a94
Fabiano Antunes <42979563+fabianoseller@users.noreply.github.com>
2024-04-12 14:54:02 -0300

    lista refeita modulos correção ....

--- aula20_2/adicionaremoveelementospct/produto/produto.py
+++ aula20_2/adicionaremoveelementospct/produto/produto.py
@@ -4,8 +4,35 @@ produto = {
     "cor": "Preto",
     "tamanho": "Médio"
 }
-print(f"Dicionário de Produto (COM a chave 'cor'): {produto}")
+
+
+
+print(f"Dicionário de Produto (COM a chave 'cor'): {produto}\n")
+
 # Removendo a chave "cor"
 del produto["cor"]
+# Dentro do módulo produto.produto
 
-print(f"Dicionário de Produto (sem a chave 'cor'): {produto}")
+def remover_cor(produto):
+    del produto["cor"]
+    return produto
+
+
+print(f"Dicionário de Produto (SEM a chave 'cor'): {produto}")
+
+# produto/produto.py
+
+produto = {
+    "nome": "Smartphone",
+    "preco": 899.99,
+    "cor": "Preto",
+    "tamanho": "Médio"
+}
+
+
+def remover_cor(produto):
+    if "cor" in produto:
+        del produto["cor"]
+    return produto
+
+
