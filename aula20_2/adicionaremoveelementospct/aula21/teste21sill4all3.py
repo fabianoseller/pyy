fd241e7310f9519ddab13b6e903839bffbaeea7e
Fabiano Antunes <182300640@senacinformatica.br>
2024-04-15 21:25:55 -0300

    Aqui arrumando modulos skil4allarquivo 3 com avaliação

---
+++ aula20_2/adicionaremoveelementospct/aula21/teste21sill4all3.py
@@ -0,0 +1,39 @@
+# x = int(input())
+# y = int(input())
+
+# x = x / y
+# y = y / x
+
+# print(y)
+
+
+# x = int(input())
+# y = int(input())
+
+# x = x % y
+# x = x % y
+# y = y % x
+
+# print(y)
+
+
+# x = input()
+# y = int(input())
+
+# print(x * y)
+
+# z = y = x = 1
+# print(x, y, z, sep='*')
+
+
+# y = 2 + 3 * 5.
+# print(Y)
+x = 1 / 2 + 3 // 3 + 4 ** 2
+print(x)
+
+x = int(input())
+y = int(input())
+
+print(x + y)
+
+
--- aula20_2/aula21skill4all2.py
+++ aula20_2/aula21skill4all2.py
@@ -2,9 +2,32 @@ print("+" + 10 * "-" + "+")
 print(("|" + " " * 10 + "|\n") * 5, end="")
 print("+" + 10 * "-" + "+")
 
+print(20.12E8)
+
+print(1 / 1)
+
+print(1 // 2 * 3)
+
+x = 1
+y = 2
+z = x
+x = y
+y = z
+print(x, y)
+
+
+x = input()
+y = input()
+print(x + y)
+
 
 x = int(input())
 y = int(input())
- 
-print(x + y)
- 
+
+x = x // y
+y = y // x
+
+print(y)
+
+
+
