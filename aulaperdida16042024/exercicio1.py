3a43976944849f250ef55f0990ec0af4d7f1ef69
Fabiano Antunes <182300640@senacinformatica.br>
2024-04-17 21:18:31 -0300

    Aula lista 1 oop

--- aulaperdida16042024/exercicio1.py
+++ aulaperdida16042024/exercicio1.py
@@ -1,3 +1,46 @@
+# class ContaBancaria:
+#     def __init__(self, titular, saldo):
+#         self.__titular = titular
+#         self.__saldo = saldo
+
+#     # Getters
+#     def get_titular(self):
+#         return self.__titular
+
+#     def get_saldo(self):
+#         return self.__saldo
+
+#     # Setters
+#     def set_titular(self, novo_titular):
+#         self.__titular = novo_titular
+
+#     def set_saldo(self, novo_saldo):
+#         self.__saldo = novo_saldo
+
+#     # Método para depositar dinheiro na conta
+#     def depositar(self, valor):
+#         self.__saldo += valor
+
+# if __name__ == "__main__":
+#     # Exemplo uma instância da classe ContaBancaria
+#     conta = ContaBancaria("João", 7000.0)
+
+#     # Exemplo Acessando e imprimindo o nome do titular da conta
+#     print("Nome do titular:", conta.get_titular())
+
+#     # Exemplo Alterando o nome do titular da conta
+#     conta.set_titular("Maria")
+
+#     # Exemplo Acessando e imprimindo o saldo da conta
+#     print("Saldo da conta:", conta.get_saldo())
+
+#     # Exemplo Depositando um valor na conta
+#     valor_deposito = 700.0
+#     conta.depositar(valor_deposito)
+
+#     # Exemplo Acessando e imprimindo o novo saldo da conta
+#     print("Novo saldo da conta após depósito de", valor_deposito, ":", conta.get_saldo())
+
 class ContaBancaria:
     def __init__(self, titular, saldo):
         self.__titular = titular
@@ -16,27 +59,15 @@ class ContaBancaria:
 
     def set_saldo(self, novo_saldo):
         self.__saldo = novo_saldo
-
-    # Método para depositar dinheiro na conta
     def depositar(self, valor):
         self.__saldo += valor
 
 if __name__ == "__main__":
-    # Criando uma instância da classe ContaBancaria
-    conta = ContaBancaria("João", 1000.0)
-
-    # Acessando e imprimindo o nome do titular da conta
+    conta = ContaBancaria("João", 7000.0)
     print("Nome do titular:", conta.get_titular())
-
-    # Alterando o nome do titular da conta
     conta.set_titular("Maria")
-
-    # Acessando e imprimindo o saldo da conta
     print("Saldo da conta:", conta.get_saldo())
-
-    # Depositando um valor na conta
-    valor_deposito = 500.0
+    valor_deposito = 700.0
     conta.depositar(valor_deposito)
-
-    # Acessando e imprimindo o novo saldo da conta
     print("Novo saldo da conta após depósito de", valor_deposito, ":", conta.get_saldo())
+
