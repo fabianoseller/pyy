606f7786aa4fd890d7754c2b55aa6a9693e01e39
Fabiano Antunes <182300640@senacinformatica.br>
2024-04-23 20:54:23 -0300

    Para aula 29 com exercicios da lista 4 e 5 NOVAERA

---
+++ aula 29/ProdutoEletronico.py
@@ -0,0 +1,27 @@
+class ProdutoEletronico:
+    def __init__(self, codigo, nome, marca, modelo, preco, tipo):
+        self.codigo = codigo
+        self.nome = nome
+        self.marca = marca
+        self.modelo = modelo
+        selfpreco = preco
+        self.tipo = tipo
+       
+    
+    def garantia_estendida(self):
+        return self.garantia_estendida + self.aumentar_preco
+    
+    def  garantia_estendida(self): FALTA CRIR OPÇÇÃO AQUI APRA VR COM FAZER "EM CONSTRUÇÃO"
+    
+    def calcula_valor_totalavalor_Totalself, percentual):
+        self.preco_passagem -= self.preco_passagem * percentual / 100
+    
+    def mostrar_informacoes(self):
+        print("Código:", self.codigo)
+        print("Nome:", self.nome)
+        print("Marca:", self.marca)
+        print("Modelo", self.modelo)
+        print("Preço:", self.preco)
+        print("Tipo:", self.tipo)
+      
+
