448396dee6077041f889b2cbff455e7dc1242a88
Fabiano Antunes <182300640@senacinformatica.br>
2024-07-03 21:02:52 -0300

    ATUALIZAÇÃO Projeto no banco de dados aula 03072024

--- senactechflask/0207/pythonlogin/main.py
+++ senactechflask/0207/pythonlogin/main.py
@@ -108,53 +108,121 @@ def register():
         email = form.email.data
         print(f"Tentando registrar usuário: {username}, {email}")
         try:
-            conexao = conecta_no_banco_de_dados()
-            cursor = conexao.cursor()
+            conexao1 = conecta_no_banco_de_dados()
+            cursor = conexao1.cursor()
             cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
             if cursor.fetchone() is not None:
                 print(f"Usuário {username} já existe.")
                 return render_template('register.html', form=form, msg='Esse usuário já existe')
             cursor.execute('INSERT INTO users (username, password, email) VALUES (%s, %s, %s)', (username, password, email))
-            conexao.commit()
+            conexao1.commit()
             print(f"Usuário {username} registrado com sucesso.")
             return redirect(url_for('login'))
         except mysql.connector.Error as err:
             print(f"Erro ao registrar usuário: {err}")
             return render_template('register.html', form=form, msg='Erro ao registrar usuário')
         finally:
-            if conexao.is_connected():
+            if conexao1.is_connected():
                 cursor.close()
-                conexao.close()
+                conexao1.close()
     return render_template('register.html', form=form)
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def contato():
-    if 'loggedin' in session:
-        form = FormularioContato()
-        if form.validate_on_submit():
-            nome = form.nome.data
-            email = form.email.data
-            mensagem = form.mensagem.data    
-            try:
-                conexao = conecta_no_banco_de_dados()
-                cursor = conexao.cursor()
-                sql = "INSERT INTO contatos (nome, email, mensagem) VALUES (%s, %s, %s)"
-                values = (nome, email, mensagem)
-                cursor.execute(sql, values)
-                conexao.commit()
-                print("Dados do formulário salvos com sucesso!")
-            except mysql.connector.Error as err:
-                print(f"Erro ao salvar dados no banco de dados: {err}")
-                mensagem_erro = "Ocorreu um erro ao processar o seu contato. Tente novamente mais tarde."
-                return render_template('erro.html', mensagem_erro=mensagem_erro), 500
-            finally:
-                if conexao.is_connected():
-                    cursor.close()
-                    conexao.close()
-            return redirect('/sucesso')
-        return render_template('contato.html', form=form)
+    form = FormularioContato()
+    # A linha form = FormularioContato() cria uma instância do formulário FormularioContato definido anteriormente.
+    # Isso torna os campos e funcionalidades do formulário disponíveis para uso dentro da função.
+
+
+
+    # A condição if form.validate_on_submit():
+    # Verifica se o formulário foi submetido e validado com sucesso.
+    # form.validate_on_submit() retorna True se todos os campos obrigatórios forem preenchidos e os valores atenderem aos critérios de validação.
+    # Se a validação falhar, o usuário verá o formulário novamente com as mensagens de erro.
+    if form.validate_on_submit():
+        nome = form.nome.data
+        email = form.email.data
+        mensagem = form.mensagem.data    
+        # As linhas nome = form.nome.data, email = form.email.data e mensagem = form.mensagem.data extraem os valores dos campos nome, email e mensagem do formulário, respectivamente.
+        # O atributo data de cada campo do formulário fornece o valor digitado pelo usuário.
+        
+        try:
+            
+            bd =conecta_no_banco_de_dados()
+            # A linha bd = conecta_no_banco_de_dados() tenta estabelecer uma conexão com o banco de dados usando a função conecta_no_banco_de_dados().
+            # Essa função, é responsável por lidar com os detalhes da conexão.
+            
+            cursor = bd.cursor()
+            sql = "INSERT INTO contatos (nome, email, mensagem) VALUES (%s, %s, %s)"
+            values = (nome, email, mensagem)
+            cursor.execute(sql, values)
+            bd.commit()
+            
+            
+
+            print(f"Dados do formulário salvos com sucesso!")
+            # Se a conexão com o banco de dados for bem-sucedida:
+            # Um cursor para executar comandos SQL é criado usando cursor = bd.cursor().
+            # A instrução SQL INSERT é construída na variável sql para inserir os dados do formulário na tabela contatos. A instrução utiliza marcadores de posição (%s) para os valores a serem inseridos.
+            # Os valores extraídos do formulário são armazenados em uma tupla values na ordem correspondente aos marcadores de posição na instrução SQL.
+            # O comando cursor.execute(sql, values) executa a instrução SQL, inserindo os dados do formulário na tabela.
+            # A função bd.commit() é chamada para confirmar as alterações feitas no banco de dados.
+            # Uma mensagem de sucesso informando que os dados foram salvos é impressa no console.
+            
+        except bd.connector.Error as err:
+            print(f"Erro ao salvar dados no banco de dados: {err}")
+
+            mensagem_erro = "Ocorreu um erro ao processar o seu contato. Tente novamente mais tarde."
+            return render_template('erro.html', mensagem_erro=mensagem_erro), 500
+            # O bloco try...except tenta executar o código de gravação no banco de dados.
+            # Se a exceção bd.connector.Error for lançada, significa que ocorreu um erro durante a interação com o banco de dados.
+            # A mensagem de erro é capturada na variável err e impressa no console para fins de depuração.
+            # Uma mensagem de erro genérica informando que houve um problema ao processar o contato é definida em mensagem_erro.
+            # A função render_template('erro.html', mensagem_erro=mensagem_erro) renderiza a página erro.html e passa a mensagem de erro como contexto.
+            # O código retorna um código de status HTTP 500, indicando um erro interno do servidor.
+        finally:
+            if bd is not None:
+                bd.close()
+                # O bloco finally é executado independentemente do sucesso ou falha da gravação no banco de dados.
+                # A linha if bd is not None: verifica se a variável bd (referência à conexão com o banco de dados) não é None.
+                # Se a conexão ainda estiver ativa, o método close() é chamado para fechá-la e liberar os recursos. 
+        return redirect('/sucesso')
+        #Se a gravação no banco de dados for bem-sucedida, a linha return redirect('/sucesso') redireciona o usuário para a rota /sucesso, que provavelmente renderiza uma página de confirmação de envio.
+
     else:
-        return redirect(url_for('login'))
+        return render_template('contato.html', form=form)
+        #Se o formulário não foi submetido ou a validação falhou, a linha return render_template('contato.html', form=form) renderiza
+
+# def contato():
+#     form = FormularioContato()
+#     if form.validate_on_submit():
+#         nome = form.nome.data
+#         email = form.email.data
+#         mensagem = form.mensagem.data    
+#         try:
+#             conexao = conecta_no_banco_de_dados()
+#             cursor = conexao.cursor()
+#             sql = "INSERT INTO contatos (nome, email, mensagem) VALUES (%s, %s, %s)"
+#             values = (nome, email, mensagem)
+#             cursor.execute(sql, values)
+#             conexao.commit()
+#             print("Dados do formulário salvos com sucesso!")
+#         except mysql.connector.Error as err:
+#             print(f"Erro ao salvar dados no banco de dados: {err}")
+#             mensagem_erro = "Ocorreu um erro ao processar o seu contato. Tente novamente mais tarde."
+#             return render_template('erro.html', mensagem_erro=mensagem_erro), 500
+#         finally:
+#             if conexao.is_connected():
+#                 cursor.close()
+#                 conexao.close()
+#                 return redirect('/sucesso')
+#     else:
+#         return render_template('contato.html', form=form)
+#         #Se o formulário não foi submetido ou a validação falhou, a linha return render_template('contato.html', form=form) renderiza        
+               
+        
 
 @app.route('/sucesso')
 def sucesso():
