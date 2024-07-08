d8d0b8c05d745db8cafcb13c1d8f2b19757a298f
Fabiano Antunes <fabianoseller@gmail.com>
2024-07-08 17:29:11 -0300

    tema relacionamento 2

--- senactechflask/08 07/pythonlogin/base_bd_config.py
+++ senactechflask/08 07/pythonlogin/base_bd_config.py
@@ -1,31 +1,6 @@
-import mysql.connector
-from mysql.connector import errorcode
-
-def conecta_no_banco_de_dados():
-    try:
-        conectacao = mysql.connector.connect(
-            host='127.0.0.1',
-            user='root',
-            password='',
-            database='pythonbas'
-        )
-        print('O banco de dados pythonbas existe e está pronto para uso.')
-        return conectacao
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_BAD_DB_ERROR:
-            criar_banco_de_dados()
-            conectacao = mysql.connector.connect(
-                host='127.0.0.1',
-                user='root',
-                password='',
-                database='pythonbas'
-            )
-            return conectacao
-        else:
-            print("Erro de conexão com o banco de dados:", err)
-            raise
-
 def criar_banco_de_dados():
+    conectacao = None
+    cursor = None
     try:
         conectacao = mysql.connector.connect(
             host='127.0.0.1',
@@ -50,10 +25,12 @@ def criar_banco_de_dados():
             CREATE TABLE IF NOT EXISTS usuarios (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 username VARCHAR(255) NOT NULL,
-                password VARCHAR(255) NOT NULL
+                password VARCHAR(255) NOT NULL,
+                nome VARCHAR(255),
+                email VARCHAR(255)
             );
         ''')
-        
+
         # Criar a tabela user_contatos com as relações especificadas
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS user_contatos (
@@ -72,12 +49,7 @@ def criar_banco_de_dados():
         print("Erro ao criar o banco de dados ou tabelas:", err)
         raise
     finally:
-        if conectacao.is_connected():
+        if cursor:
             cursor.close()
+        if conectacao and conectacao.is_connected():
             conectacao.close()
-
-# Testar a função de conexão
-conexao = conecta_no_banco_de_dados()
-if conexao.is_connected():
-    print("Conexão estabelecida com sucesso!")
-    conexao.close()
